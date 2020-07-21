9aef5c20596e931ccb421d4ec4a9e5e5f688f867
Davide Sanvito <dsanvito90@gmail.com>
2020-07-21 08:47:39 +0200

    indentation fix

--- gui/www/gui.js
+++ gui/www/gui.js
@@ -11,183 +11,187 @@ function escapeHtml(text) {
 }
 
 function addMatch(){
-    var match = document.getElementById("matchesList");
-    var matchField = document.getElementById("matchField");
-    var matchFieldText = matchField.options[matchField.selectedIndex].text;
-    var matchValueText = document.getElementById("matchValue").value.trim();
-    if (matchField.selectedIndex == 0) {
-      alert("Please select a match field!")
-      return;
-    }
-    if (matchValueText.length == 0) {
-      alert("Please specify a non-empty match value!")
-      return;
-    }
-    var input = document.createElement("label");
-    input.value = matchFieldText + " == " + matchValueText;
-    input.innerHTML = matchFieldText + " == " + matchValueText;
-    input.className = "matchDiv";
-    match.appendChild(input);
-    // clear match
-    matchField.selectedIndex = 0;
-    document.getElementById("matchValue").value = "";
+  //TODO validate input
+  //TODO how to define non-exact matches? Like in P4?
+  var match = document.getElementById("matchesList");
+  var matchField = document.getElementById("matchField");
+  var matchFieldText = matchField.options[matchField.selectedIndex].text;
+  var matchValueText = document.getElementById("matchValue").value.trim();
+  if (matchField.selectedIndex == 0) {
+    alert("Please select a match field!")
+    return;
   }
-           
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  function addCondition(){
-    var conditionOperator = document.getElementById("condOperator");
-    var condOperand1Text = document.getElementById("condOperand1").value.trim();
-    var condOperand2Text = document.getElementById("condOperand2").value.trim();
-    if (conditionOperator.selectedIndex == 0) {
-      alert("Please select an operator!")
-      return;
-    }
-    if (condOperand1Text.length == 0) {
-      alert("Please insert a non-empty operand 1!")
-      return;
-    }
-    if (condOperand2Text.length == 0) {
-      alert("Please insert a non-empty operand 2!")
-      return;
-    }
-    var condition = document.getElementById("conditionsList");
-    var conditionOperatorText = conditionOperator.options[conditionOperator.selectedIndex].text;
-    var input = document.createElement("label");
-    input.value = condOperand1Text + " " + conditionOperatorText + " " + condOperand2Text;
-    input.innerHTML = escapeHtml(condOperand1Text + " " + conditionOperatorText + " " + condOperand2Text);
-    input.className = "conditionDiv";
-    condition.appendChild(input);
-    // clear condition
-    conditionOperator.selectedIndex = 0;
-    document.getElementById("condOperand1").value = "";
-    document.getElementById("condOperand2").value = "";
+  if (matchValueText.length == 0) {
+    alert("Please specify a non-empty match value!")
+    return;
   }
-           
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+  var input = document.createElement("label");
+  input.value = matchFieldText + " == " + matchValueText;
+  input.innerHTML = matchFieldText + " == " + matchValueText;
+  input.className = "matchDiv";
+  match.appendChild(input);
+  // clear match
+  matchField.selectedIndex = 0;
+  document.getElementById("matchValue").value = "";
+}
 
-  function addUpdate(){
-    var updateOperation = document.getElementById("updateOperation");
-    var updateOutputText = document.getElementById("updateOutput").value.trim();
-    var updateOperand1Text = document.getElementById("updateOperand1").value.trim();
-    var updateOperand2Text = document.getElementById("updateOperand2").value.trim();
-    if (updateOperation.selectedIndex == 0 && updateOperand2Text.length > 0) {
-      alert("Please select an operator!");
-      return;
-    }
-    if (document.getElementById("updateOutput").value == "") {
-      alert("Please insert a non-empty output!")
-      return;
-    }
-    if (updateOperand1Text.length == 0) {
-      alert("Please insert a non-empty operand 1!")
-      return;
-    }
-    if (updateOperand2Text.length == 0 && updateOperation.selectedIndex > 0) {
-      alert("Please insert a non-empty operand 2!")
-      return;
-    }
-    var condition = document.getElementById("updateList");
-    var updateOperationText = updateOperation.options[updateOperation.selectedIndex].text;
-    var input = document.createElement("label");
-    if (updateOperation.selectedIndex == 0) {
-      input.value = document.getElementById("updateOutput").value + " = " + updateOperand1Text;
-      input.innerHTML = document.getElementById("updateOutput").value + " = " + updateOperand1Text;
-    } else {
-      input.value = document.getElementById("updateOutput").value + " = " + updateOperand1Text + " " + updateOperationText + " " + updateOperand2Text;
-      input.innerHTML = document.getElementById("updateOutput").value + " = " + updateOperand1Text + " " + updateOperationText + " " + updateOperand2Text;
-    }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    input.className = "updateDiv";
-    condition.appendChild(input);
-    // clear update
-    updateOperation.selectedIndex = 0;
-    document.getElementById("updateOutput").value = "";
-    document.getElementById("updateOperand1").value = "";
-    document.getElementById("updateOperand2").value = "";
+function addCondition(){
+  //TODO valide input: strings are variables, numbers are constants
+  var conditionOperator = document.getElementById("condOperator");
+  var condOperand1Text = document.getElementById("condOperand1").value.trim();
+  var condOperand2Text = document.getElementById("condOperand2").value.trim();
+  if (conditionOperator.selectedIndex == 0) {
+    alert("Please select an operator!")
+    return;
   }
-           
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  function setAction(){
-    var action = document.getElementById("action");
-    if (action.selectedIndex == 0) {
-      alert("Please select an action!");
-      return;
-    }
-    var actionList = document.getElementById("actionList");
-    var actionText = action.options[action.selectedIndex].text;
-    var actionParamText = document.getElementById("actionParam").value.trim();
-    var actionParamTextNum = actionParamText.split(',').length;
-
-    var actionParamStr = actionText.match(/.*\((.*)\)/)[1];
-    var actionParamNum = actionParamStr.split(',').length;
-    if (actionParamStr.length == 0 && actionParamText.length > 0) {
-      alert("The selected action do not expect any parameter!");
-      return;
-    }
-    if (actionParamStr.length > 0 && actionParamNum == 1 && (actionParamText.length == 0 || actionParamTextNum > 1)) {
-      alert("The selected action expects one parameter!");
-      return;
-    }
-    if (actionParamStr.length > 0 && actionParamTextNum != actionParamNum) {
-      alert("The selected action expects " + actionParamNum + " comma separated parameters!");
-      return;
-    }
-
-    var input = document.createElement("label");
-    input.value = document.getElementById("action").value + "(" + actionParamText + ")";
-    input.innerHTML = document.getElementById("action").value + "(" + actionParamText + ")";
-    input.className = "actionDiv";
-    actionList.appendChild(input);
-    // clear update
-    action.selectedIndex = 0;
-    document.getElementById("actionParam").value = "";
+  if (condOperand1Text.length == 0) {
+    alert("Please insert a non-empty operand 1!")
+    return;
   }
-           
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+  if (condOperand2Text.length == 0) {
+    alert("Please insert a non-empty operand 2!")
+    return;
+  }
+  var condition = document.getElementById("conditionsList");
+  var conditionOperatorText = conditionOperator.options[conditionOperator.selectedIndex].text;
+  var input = document.createElement("label");
+  input.value = condOperand1Text + " " + conditionOperatorText + " " + condOperand2Text;
+  input.innerHTML = escapeHtml(condOperand1Text + " " + conditionOperatorText + " " + condOperand2Text);
+  input.className = "conditionDiv";
+  condition.appendChild(input);
+  // clear condition
+  conditionOperator.selectedIndex = 0;
+  document.getElementById("condOperand1").value = "";
+  document.getElementById("condOperand2").value = "";
+}
 
-  function createTransitionString(){
-    var children = document.getElementById("matchesList").children;
-    var out = document.getElementById("out");
-    out.value = "";
-    for (var i = 0; i < children.length; i++) {
-      out.value += children[i].value;
-      if (i < children.length - 1){
-        out.value += " ; ";
-      }
-    }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    out.value += " | ";
+function addUpdate(){
+  //TODO valide input: strings are variables, numbers are constants
+  var updateOperation = document.getElementById("updateOperation");
+  var updateOutputText = document.getElementById("updateOutput").value.trim();
+  var updateOperand1Text = document.getElementById("updateOperand1").value.trim();
+  var updateOperand2Text = document.getElementById("updateOperand2").value.trim();
+  if (updateOperation.selectedIndex == 0 && updateOperand2Text.length > 0) {
+    alert("Please select an operator!");
+    return;
+  }
+  if (document.getElementById("updateOutput").value == "") {
+    alert("Please insert a non-empty output!")
+    return;
+  }
+  if (updateOperand1Text.length == 0) {
+    alert("Please insert a non-empty operand 1!")
+    return;
+  }
+  if (updateOperand2Text.length == 0 && updateOperation.selectedIndex > 0) {
+    alert("Please insert a non-empty operand 2!")
+    return;
+  }
+  var condition = document.getElementById("updateList");
+  var updateOperationText = updateOperation.options[updateOperation.selectedIndex].text;
+  var input = document.createElement("label");
+  if (updateOperation.selectedIndex == 0) {
+    input.value = document.getElementById("updateOutput").value + " = " + updateOperand1Text;
+    input.innerHTML = document.getElementById("updateOutput").value + " = " + updateOperand1Text;
+  } else {
+    input.value = document.getElementById("updateOutput").value + " = " + updateOperand1Text + " " + updateOperationText + " " + updateOperand2Text;
+    input.innerHTML = document.getElementById("updateOutput").value + " = " + updateOperand1Text + " " + updateOperationText + " " + updateOperand2Text;
+  }
 
-    children = document.getElementById("conditionsList").children;
-    for (var i = 0; i < children.length; i++) {
-      out.value += children[i].value;
-      if (i < children.length - 1){
-        out.value += " ; ";
-      }
-    }
+  input.className = "updateDiv";
+  condition.appendChild(input);
+  // clear update
+  updateOperation.selectedIndex = 0;
+  document.getElementById("updateOutput").value = "";
+  document.getElementById("updateOperand1").value = "";
+  document.getElementById("updateOperand2").value = "";
+}
 
-    out.value += " | ";
-    
-    children = document.getElementById("updateList").children;
-    for (var i = 0; i < children.length; i++) {
-      out.value += children[i].value;
-      if (i < children.length - 1){
-        out.value += " ; ";
-      }
-    }
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    out.value += " | ";
-    
-    children = document.getElementById("actionList").children;
-    for (var i = 0; i < children.length; i++) {
-      out.value += children[i].value;
-      if (i < children.length - 1){
-        out.value += " ; ";
-      }
+function setAction(){
+  var action = document.getElementById("action");
+  if (action.selectedIndex == 0) {
+    alert("Please select an action!");
+    return;
+  }
+  var actionList = document.getElementById("actionList");
+  var actionText = action.options[action.selectedIndex].text;
+  var actionParamText = document.getElementById("actionParam").value.trim();
+  var actionParamTextNum = actionParamText.split(',').length;
+
+  var actionParamStr = actionText.match(/.*\((.*)\)/)[1];
+  var actionParamNum = actionParamStr.split(',').length;
+  if (actionParamStr.length == 0 && actionParamText.length > 0) {
+    alert("The selected action do not expect any parameter!");
+    return;
+  }
+  if (actionParamStr.length > 0 && actionParamNum == 1 && (actionParamText.length == 0 || actionParamTextNum > 1)) {
+    alert("The selected action expects one parameter!");
+    return;
+  }
+  if (actionParamStr.length > 0 && actionParamTextNum != actionParamNum) {
+    alert("The selected action expects " + actionParamNum + " comma separated parameters!");
+    return;
+  }
+
+  var input = document.createElement("label");
+  input.value = document.getElementById("action").value + "(" + actionParamText + ")";
+  input.innerHTML = document.getElementById("action").value + "(" + actionParamText + ")";
+  input.className = "actionDiv";
+  actionList.appendChild(input);
+  // clear update
+  action.selectedIndex = 0;
+  document.getElementById("actionParam").value = "";
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function createTransitionString(){
+  var children = document.getElementById("matchesList").children;
+  var out = document.getElementById("out");
+  out.value = "";
+  for (var i = 0; i < children.length; i++) {
+    out.value += children[i].value;
+    if (i < children.length - 1){
+      out.value += " ; ";
     }
   }
+
+  out.value += " | ";
+
+  children = document.getElementById("conditionsList").children;
+  for (var i = 0; i < children.length; i++) {
+    out.value += children[i].value;
+    if (i < children.length - 1){
+      out.value += " ; ";
+    }
+  }
+
+  out.value += " | ";
+  
+  children = document.getElementById("updateList").children;
+  for (var i = 0; i < children.length; i++) {
+    out.value += children[i].value;
+    if (i < children.length - 1){
+      out.value += " ; ";
+    }
+  }
+
+  out.value += " | ";
+
+  children = document.getElementById("actionList").children;
+  for (var i = 0; i < children.length; i++) {
+    out.value += children[i].value;
+    if (i < children.length - 1){
+      out.value += " ; ";
+    }
+  }
+}
 
 function clearTransitionString(){
   var myNode = document.getElementById("matchesList");
@@ -224,11 +228,12 @@ function generateP4(){
   };
   xhr.send(localStorage['fsm']);
 }
+
 function saveBlob(blob, fileName) {
-    var a = document.createElement('a');
-    a.href = window.URL.createObjectURL(blob);
-    a.download = fileName;
-    a.dispatchEvent(new MouseEvent('click'));
+  var a = document.createElement('a');
+  a.href = window.URL.createObjectURL(blob);
+  a.download = fileName;
+  a.dispatchEvent(new MouseEvent('click'));
 }
 
 function loadSampleFSMrate(){
