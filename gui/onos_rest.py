4b2a8b3f724ccc6085fa932007e5b594fb16a3d0
Daniele Moro <daniele@opennetworking.org>
2020-10-12 16:12:29 +0200

    Update GUI for pushing to ONOS the FlowBlaze dev ID

--- gui/onos_rest.py
+++ gui/onos_rest.py
@@ -20,13 +20,23 @@ import requests
 
 DEFAULT_ONOS_USER = "onos"
 DEFAULT_ONOS_PASS = "rocks"
-DEFAULT_ONOS_IP = "onos" # default when running with compose
+DEFAULT_ONOS_IP = "onos"  # default when running with compose
 DEFAULT_ONOS_PORT = 8181
+DEFAULT_FLOWBLAZE_DEVICE_ID = "device:leaf1"
+
+
+def config_flowblaze_device_id(flowblaze_device_id, onos_ip, onos_port):
+    logging.debug("Set FlowBlaze device ID: %s" % flowblaze_device_id)
+    code = json_get_req('http://%s:%d/onos/flowblaze/setDeviceId/%s' % (onos_ip, onos_port, flowblaze_device_id))
+    if code != 200:
+        return False
+    return True
 
 
 def push_efsm_table(efsm_table_dict, onos_ip, onos_port):
     logging.debug("Efsm Entry: %s" % str(efsm_table_dict))
-    code = json_post_req(('http://%s:%d/onos/flowblaze/setEfsmEntry' % (onos_ip, onos_port)), json.dumps(efsm_table_dict))
+    code = json_post_req(('http://%s:%d/onos/flowblaze/setEfsmEntry' % (onos_ip, onos_port)),
+                         json.dumps(efsm_table_dict))
     if code != 200:
         return False
     return True
@@ -34,7 +44,8 @@ def push_efsm_table(efsm_table_dict, onos_ip, onos_port):
 
 def push_conditions(conditions_dict, onos_ip, onos_port):
     logging.debug("Conditions: %s" % str(conditions_dict))
-    code = json_post_req(('http://%s:%d/onos/flowblaze/setConditions' % (onos_ip, onos_port)), json.dumps(conditions_dict))
+    code = json_post_req(('http://%s:%d/onos/flowblaze/setConditions' % (onos_ip, onos_port)),
+                         json.dumps(conditions_dict))
     if code != 200:
         return False
     return True
@@ -42,7 +53,8 @@ def push_conditions(conditions_dict, onos_ip, onos_port):
 
 def push_pkt_actions(pkt_actions_dict, onos_ip, onos_port):
     logging.debug("Packet Action: %s" % str(pkt_actions_dict))
-    code = json_post_req(('http://%s:%d/onos/flowblaze/setPktActions' % (onos_ip, onos_port)), json.dumps(pkt_actions_dict))
+    code = json_post_req(('http://%s:%d/onos/flowblaze/setPktActions' % (onos_ip, onos_port)),
+                         json.dumps(pkt_actions_dict))
     if code != 200:
         return False
     return True
@@ -51,8 +63,23 @@ def push_pkt_actions(pkt_actions_dict, onos_ip, onos_port):
 def json_post_req(url, json_data):
     try:
         headers = {'Content-Type': 'application/json', 'Accept': 'application/json'}
-        response = requests.post(url, data=json_data,
-                                 auth=(DEFAULT_ONOS_USER, DEFAULT_ONOS_PASS), headers=headers, timeout=0.5)
+        response = requests.post(url,
+                                 data=json_data,
+                                 auth=(DEFAULT_ONOS_USER, DEFAULT_ONOS_PASS), headers=headers,
+                                 timeout=0.5)
+        logging.debug(response.text)
+        return response.status_code
+    except IOError as e:
+        logging.error(str(e))
+        return 500
+
+
+def json_get_req(url, params=None):
+    try:
+        response = requests.get(url,
+                                params=params,
+                                auth=(DEFAULT_ONOS_USER, DEFAULT_ONOS_PASS),
+                                timeout=0.5)
         logging.debug(response.text)
         return response.status_code
     except IOError as e:
